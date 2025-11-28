d6b34a9fdaa1733f1cd26253baba4190515d436a
prudvikumara <prudhvikumar538@gmail.com>
2025-11-28 10:47:19 +0530

    service created

--- app/src/main/java/com/example/simulatorservice/SimulatorService.kt
+++ app/src/main/java/com/example/simulatorservice/SimulatorService.kt
@@ -6,6 +6,7 @@ import android.os.IBinder
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import kotlinx.coroutines.CoroutineScope
+
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
